6ba29e559dd9930de5b70624487a11a559dc2f88
samuelsilvabezerrajunior <samuelsilvabezerrajunior@gmail.com>
2025-04-06 23:09:28 -0300

    update

--- tools/monitor.ps1
+++ tools/monitor.ps1
@@ -1,11 +1,12 @@
 # Configuração do executável e do intervalo de monitoramento
 $executablePath = "C:\Users\samue\source\repos\SamuelSBJr97\net-4.x-client-rest-test\src\client\ApiClient46.Test\bin\Debug\net462\ApiClient46.Test.exe"
 $logFilePath = "C:\Users\samue\source\repos\SamuelSBJr97\net-4.x-client-rest-test\tools\monitor_log.txt"
-$executableLogFile = "C:\Users\samue\source\repos\SamuelSBJr97\net-4.x-client-rest-test\tools\executable_output_log.txt"
+$stdoutLogFile = "C:\Users\samue\source\repos\SamuelSBJr97\net-4.x-client-rest-test\tools\executable_stdout_log.txt"
+$stderrLogFile = "C:\Users\samue\source\repos\SamuelSBJr97\net-4.x-client-rest-test\tools\executable_stderr_log.txt"
 $intervalSeconds = 300 # 5 minutos
 
-# Iniciar o executável e redirecionar saída para o arquivo de log
-Start-Process -FilePath $executablePath -NoNewWindow -RedirectStandardOutput $executableLogFile -RedirectStandardError $executableLogFile
+# Iniciar o executável e redirecionar saída padrão e de erro para arquivos separados
+Start-Process -FilePath $executablePath -NoNewWindow -RedirectStandardOutput $stdoutLogFile -RedirectStandardError $stderrLogFile
 
 # Função para obter informações de uso de CPU e memória
 function Get-ProcessInfo {
